4b503591ad7ae1b58000b7a38b07563f6ab32c1e
Daniel Maric <danielmaric48@gmail.com>
2025-10-25 18:16:58 +0200

    change

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ export const HeroSection = () => {
                     <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Marić</span>
                 </h1>
 
-                <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">Put your text here</p>
+                <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">I'm a junior Web Developer</p>
                 <div className="pt-4 opacity-0 animate-fade-in-delay-4">
                     <a href="#projects" className="cosmic-button">
                         View My Work
